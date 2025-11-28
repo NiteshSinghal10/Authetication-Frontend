72d6b92937cea59dc2983459ad031eed1dd4eb71
Nitesh <nitesh2@relinns.com>
2025-11-28 16:46:16 +0530

    [FIXED]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet, ActivatedRoute } from '@angular/router';
+import { RouterOutlet, ActivatedRoute, Router } from '@angular/router';
 
 import { environment } from '../environments/environment';
 import { LoginService } from '../app/services';
@@ -10,19 +10,29 @@ import { LoginService } from '../app/services';
 })
 export class AppComponent implements OnInit {
   googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=1047484772005-2d8ekhh9ehcabiur6tordtqtfu1nlnig.apps.googleusercontent.com&redirect_uri=${environment.frontendBaseUrl}&response_type=code&scope=openid%20email%20profile%20https://www.googleapis.com/auth/user.gender.read%20https://www.googleapis.com/auth/user.birthday.read&access_type=offline&prompt=consent`;
+  aud = 'vib3ly.shop';
+  deviceType: 'WEB' | 'ANDROID' | 'IOS' = 'WEB';
 
   constructor (
     private loginService: LoginService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
 
   ngOnInit(): void {
     this.route.queryParamMap.subscribe(params => {
       const code = params.get('code');
+      const aud = params.get('code') ?? this.aud;
+      const deviceType: 'WEB' | 'ANDROID' | 'IOS' = (params.get('deviceType') as 'WEB' | 'ANDROID' | 'IOS' | null) ?? this.deviceType;
+      const redirectUri = params.get('redirectUri') ?? '';
 
       if (code) {
-        this.loginService.codeExchange(String(code), 'Nitesh.com', 'WEB').subscribe(res => console.log("step 2:", res));
+        this.loginService.codeExchange(String(code), aud, deviceType).subscribe(res => {
+          if(redirectUri) {
+            window.location.assign(redirectUri)
+          }
+        });
       }
     });
   }
